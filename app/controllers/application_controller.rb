602e01e7fff4a8efda5e8f9dfacdb863c68a7127
Harsha <hrshgopal@gmail.com>
2018-02-17 17:37:41 +0530

    pro

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   test app
-  Noob me
+  Noob measdasd
+  assert_difference "assert_differencead
+  assert_differenceadd
+  assert_difference "asdas
+  def asd
+  	asd
+  	a
+  	das
+  	dasdas
+  	dasdas
+  	dasdasd
+  	
+  end.count", 1 do
+    
+  end.count", 1 do
+    
+  end
 end
